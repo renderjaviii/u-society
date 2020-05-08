ee78757e1cf682c44aebe7b909d2a5def0c3ac53
Ardila Javier <aj.ardila@transportsystems.co>
2020-05-08 18:08:44 -0500

    Docker + entrypoints added.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,3 @@
-CREATE TABLE IF NOT EXISTS `config` (
-    `name` varchar(255) DEFAULT NULL,
-    `value` varchar(255) DEFAULT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8;
-
 INSERT INTO config(name, value) VALUES ('config.access-token.signing-key', '123');
 INSERT INTO config(name, value) VALUES ('config.access-token.validity-seconds', '43200');
 INSERT INTO config(name, value) VALUES ('config.grant.types', 'password,client_credentials,refresh_token');
@@ -23,4 +18,4 @@ INSERT INTO role_privilege(role_id, privilege_id) VALUES (2, 2);
 INSERT INTO credential(id, client_id, client_secret, created_at, credentials_expired, description, grant_type, `scope`) VALUES (1, 'clientIdTest', '$2a$10$Pz5yIaKO/JwGQ0pio1XLs.xDPrPv95SO0F2A3BCKV9USuD1xqUlZ.', '2020-04-10', 0, "Credential for testing.", 'password,client_credentials,refresh_token', 'WEB');
 
 -- User for testing -> username: renderjaviii, password: Password123
-INSERT INTO `user`(id, account_locked, birth_date, created_at, document_number, first_name, gender, last_access_at, last_name, password, phone_number, username, role_id) VALUES (1, 0, '1998-06-03', '2020-04-11', '1015475241', 'Javier', 'M', '2020-04-11 22:22:38', 'Ardila', '$2a$10$EhhFZK1Rq8Zz31guk56vJOJXIOEyngnwhN/QdOFKMjpDA5ljhtdAG', '3146628827', 'renderjaviii', 1);
+INSERT INTO `user` (id, account_locked, birth_date, created_at, document_number, email, email_verified, first_name, gender, last_access_at, last_name, password, phone_number, username, role_id) VALUES(1, 0, '1998-06-03', '2020-04-11', '1015475241', 'example@domain.co', 1, 'Javier', 'M', '2020-04-11 22:22:38.0', 'Ardila', '$2a$10$EhhFZK1Rq8Zz31guk56vJOJXIOEyngnwhN/QdOFKMjpDA5ljhtdAG', '3146628827', 'renderjaviii', 1);
