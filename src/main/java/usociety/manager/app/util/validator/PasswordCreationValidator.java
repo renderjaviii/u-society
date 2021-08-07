b7082de56559c0de276a0153c86499e10b9e07e7
renderjaviii <renderjavi1998@gmail.com>
2021-08-06 19:47:23 -0500

    Constant renamed.

--- src/main/java/usociety/manager/app/util/validator/PasswordCreationValidator.java
+++ src/main/java/usociety/manager/app/util/validator/PasswordCreationValidator.java
@@ -7,12 +7,12 @@ import org.apache.commons.lang3.Validate;
 
 public class PasswordCreationValidator implements ConstraintValidator<PasswordCreationConstraint, String> {
 
-    private static final String PASSWORD_REGEX = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$";
+    private static final String REGEX = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$";
 
     @Override
     public boolean isValid(String password, ConstraintValidatorContext context) {
         try {
-            Validate.matchesPattern(password, PASSWORD_REGEX);
+            Validate.matchesPattern(password, REGEX);
         } catch (IllegalArgumentException ex) {
             return false;
         }
